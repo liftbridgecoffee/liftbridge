91e1bd1342afe3955f0fe7713886ec4d4fc77150
Colin Schlecht <schlechtcolin@yahoo.com>
2022-02-03 22:34:56 -0800

    establishes menu cards and base styling

--- Components/MenuComponents/MenuCard.js
+++ Components/MenuComponents/MenuCard.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import can from "../../public/LiftBridgeCoffeeManifesto.png"
+
+import mCardStyle from "../../styles/MenuCard.module.css";
 
 const MenuCard = ({ menu }) => {
-	console.log(menu);
+
+	const title = menu.title.toUpperCase()
+
 	return (
-		<div>
-			<Link href={`/menu/${menu.sheetName}`}>
-				<h1>{menu.title}</h1>
+		<div className={mCardStyle.foundation}>
+
+			<Link href={`/menu/${menu.sheetName}`} passHref>
+				<a href="replace">
+					<span className={mCardStyle.overlay}>
+						<h2 className={mCardStyle.label}>{title}</h2>
+					</span>
+				</a>
 			</Link>
+			
+			<Image
+					className={mCardStyle.image}
+					alt="Delicious Lift Bridge Coffee"
+					quality={100}
+					src={can}
+					//layout="fill"
+					objectFit="cover"
+					placeholder="blur"
+				/>
 		</div>
 	);
 };
