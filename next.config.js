981abd310f808be6d1c9129a1a3e8deb70c3870b
Colin Schlecht <schlechtcolin@yahoo.com>
2022-08-24 23:51:09 -0700

    expanding config for google auth

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ module.exports = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	webpack: (config) => {
+		config.node = {
+			fs: "empty",
+			child_process: "empty",
+			net: "empty",
+			dns: "empty",
+			tls: "empty",
+		};
+		return config;
+	},
 };
